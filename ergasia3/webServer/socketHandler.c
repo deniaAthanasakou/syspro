4538a7ee28ca908f2c0d052068f281e1d4409a2b
deniaAthanasakou <antonia.ath@gmail.com>
2018-06-04 13:00:20 +0300

    douleuoun response kai cmd dn exw datetime yet

--- ergasia3/webServer/socketHandler.c
+++ ergasia3/webServer/socketHandler.c
@@ -10,7 +10,8 @@
 #include <ctype.h> /* toupper */
 #include <signal.h> /* signal */
 #include <time.h>
-
+#include <sys/select.h>
+#include <sys/time.h>
 #include "socketHandler.h"
 #include "errorHandler.h"
 #include "pageHandler.h"
@@ -20,7 +21,7 @@
 #define BUFFSIZE 4096
 
 
-void createSocket(int servingPort, int commandPort, char* rootDirectory){
+void createSocket(int servingPort, int commandPort, char* rootDirectory, struct timeval* begin){
 
 	int my_socket, my_new_socket;
 	struct sockaddr_in server, client;
@@ -49,33 +50,118 @@ void createSocket(int servingPort, int commandPort, char* rootDirectory){
 	if (listen(my_socket, 5) < 0){						//5 is default and represents queue_length
 		perror_exit("Listening");
 	}
+
+
+	int my_CommandSocket, my_new_CommandSocket;
+	struct sockaddr_in serverForCommand;
+	struct sockaddr *serverptrForCommand=(struct sockaddr *)&serverForCommand;
+
+
+	if ((my_CommandSocket = socket(AF_INET, SOCK_STREAM, 0)) < 0){			//socket creation
+		perror_exit("Socket creation");
+	}
+
+	serverForCommand.sin_family = AF_INET; /* Internet domain */
+	serverForCommand.sin_addr.s_addr = htonl(INADDR_ANY);
+	serverForCommand.sin_port = htons(commandPort); /* The given port */
+
+
+	/* Bind socket to address */
+	if (bind(my_CommandSocket, serverptrForCommand, sizeof(serverForCommand)) < 0){
+		perror_exit("Binding");
+	}
+
+	/* Listen for connections */
+	if (listen(my_CommandSocket, 5) < 0){						//5 is default and represents queue_length
+		perror_exit("Listening");
+	}
+
+
 	printf("Listening for connections to port %d\n", servingPort);
-	int stopWhile=0;
-	while (!stopWhile) { 
-		clientlen = sizeof(client);
-		/* accept connection */
+	printf("Listening for connections to command port %d\n", commandPort);
 
-		if ((my_new_socket = accept(my_socket, clientptr, &clientlen))< 0){				//accepts connection with clientptr
-			perror_exit("Accepting connection");
+	//initialization of fd_set
+	fd_set* myFdSet= malloc(sizeof(fd_set));
+	clientlen = sizeof(client);
+	Stats* stats = malloc(sizeof(Stats));
+	initializeStats(stats);
+
+
+	while (1) { 
+		
+		FD_ZERO(myFdSet); /* clear all bits in fdset */
+		FD_SET(my_socket, myFdSet); /* turn on bit for fd my_socket */
+		FD_SET(my_CommandSocket, myFdSet); /* turn on bit for fd my_CommandSocket */
+		int nfds=0;
+		if(my_socket>my_CommandSocket){
+			nfds=my_socket+1;
+		}
+		else{
+			nfds=my_CommandSocket+1;
+		}
+		if(select(nfds, myFdSet, NULL, NULL, NULL)<0){
+			printf("error in select\n");
+			perror_exit("Select");
+		}	
+
+		if(FD_ISSET(my_CommandSocket, myFdSet)){		//get command
+			/* accept connection */
+			if ((my_new_CommandSocket = accept(my_CommandSocket, clientptr, &clientlen))< 0){				//accepts connection with clientptr
+				perror_exit("Accepting connection with telnet");
+			}
+			char command[15];
+			int commandLength=0;
+			if((commandLength=read(my_new_CommandSocket, command, 15)) > 0){
+				if(commandLength>=2){
+					command[commandLength-2]='\0';
+					printf("COMMAND '%s'\n", command);
+
+					if(!strcmp(command, "STATS")){
+						printf("in STATS\n");
+						struct timeval end;
+						gettimeofday(&end, NULL); 
+
+						unsigned int t = (end.tv_usec - begin->tv_usec)/1000;	// /1000 to get milliseconds
+						printf("millisecs %d\n", t);
+
+						printf("served %d pages, %ld bytes\n", stats->pagesServed, stats->bytes);
+						initializeStats(stats);
+
+					}
+					else if(!strcmp(command, "SHUTDOWN")){
+						printf("in SHUTDOWN\n"); 
+						//send signal to all threads to stop
+						break;
+					}
+					else{
+						printf("ignore\n"); 
+					}
+				}
+			}
 		}
 
-		/* Find client's name */
-		if ((rem = gethostbyaddr((char *) &client.sin_addr.s_addr,sizeof(client.sin_addr.s_addr), client.sin_family))== NULL) {
-			perror_exit("Could not find client's name. Error in gethostbyaddr");
-		}
-		printf("Accepted connection from %s\n", rem->h_name);
+		if(FD_ISSET(my_socket, myFdSet)){			//get request from crawler			/* accept connection */
 
-		int stopWhile = readFromSocket(my_new_socket, rootDirectory);
+			/* accept connection */
+			if ((my_new_socket = accept(my_socket, clientptr, &clientlen))< 0){				//accepts connection with clientptr
+				perror_exit("Accepting connection");
+			}
+
+			readFromSocket(my_new_socket, rootDirectory, stats);
+		}
 
 		
 	}
 	close(my_socket); /* parent closes socket to client */
 	//close(my_new_socket); /* parent closes socket to client */ //isws na mh xreiazetai
-
+	close(my_CommandSocket); /* parent closes socket to client */
+	close(my_new_CommandSocket); /* parent closes socket to client */ //isws na mh xreiazetai
+	free(myFdSet);
 	printf("Everything is ok.\n");
+	free(stats);
 }
 
-int readFromSocket(int newSocket, char* rootDirectory) {
+int readFromSocket(int newSocket, char* rootDirectory, Stats* stats) {
 	char request[1024];
 	char buffer[BUFFSIZE+1];
 	
@@ -109,7 +195,7 @@ int readFromSocket(int newSocket, char* rootDirectory) {
 		}
 
 		
-		char* response = handleRequest(request, rootDirectory);
+		char* response = handleRequest(request, rootDirectory, stats);
 		
 		//printf("content = %sOPK\n", response);
 
@@ -184,7 +270,7 @@ char* getResponse(char* firstFline, int contentLength, char* content){
 	return response;
 }
 
-char* handleRequest(char* req, char* rootDirectory){	//will check for line with GET and line with Host
+char* handleRequest(char* req, char* rootDirectory, Stats* stats){	//will check for line with GET and line with Host
 	char* response=NULL;
 
 	char* initialReq=malloc((strlen(req)+1)*sizeof(char));
@@ -218,6 +304,13 @@ char* handleRequest(char* req, char* rootDirectory){	//will check for line with 
 		else{			//check page
 			printf("page is '%s'\n", page);
 			ResponseStr* responseStr = getResponseStrOfPage(page, rootDirectory);
+
+
+			if(!strcmp(responseStr->firstLine, "HTTP/1.1 200 OK")){
+				stats->pagesServed++;
+				stats->bytes+=responseStr->contentLength;
+			}
+
 
 			response = getResponse(responseStr->firstLine, responseStr->contentLength, responseStr->content);
 			free(responseStr->content);
@@ -293,5 +386,7 @@ char* getResponseForBadRequest(){
 }
 
 
-
-
+void initializeStats(Stats* stats){
+	stats->pagesServed=0;
+	stats->bytes=0;
+}
